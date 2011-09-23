301ace2b8fe9566ec0675da39ebc935a664f24c2
nherment <nherment@modeln.com>
2011-09-22 19:08:57 -0700

    new stuff

--- Sybeol.js
+++ Sybeol.js
@@ -1,126 +1,33 @@
-var mongoose = require('mongoose');
-var crypto = require('crypto');
 
-mongoose.connect('mongodb://localhost/dev_database');
+var uuid = require('node-uuid');
+var authSvc = require('./authentication.js');
+var db = require('./db.js');
 
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+var User = db.User;
+var Measure = db.Measure;
+var createUser = db.createUser;
+var createMeasure = db.createMeasure;
 
-
-function validatePresenceOf(value) {
-  return value && value.length;
-}
-
-UserSchema = new Schema({
-  'email': { type: String, validate: [validatePresenceOf, 'an email is required'], index: { unique: true } },
-  'hashed_password': String,
-  'salt': String
-});
-
-UserSchema.virtual('password')
-  .set(function(password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function() { return this._password; });
-
-UserSchema.pre('save', function(next) {
-  if (!validatePresenceOf(this.password)) {
-    next(new Error('Invalid password'));
-  } else {
-    next();
-  }
-});
-
-UserSchema.method('authenticate', function(plainText) {
-  return this.encryptPassword(plainText) === this.hashed_password;
-});
-
-UserSchema.method('makeSalt', function() {
-  return Math.round((new Date().valueOf() * Math.random())) + '';
-});
-
-UserSchema.method('encryptPassword', function(password) {
-  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-});
-
-var User = mongoose.model('User', UserSchema);
-
-
-var MeasureSchema = new Schema({
-    user    :  [User]
-  , type    :  { type: String, index: true }
-  , time    :  { type: Date, default: Date.now, index: true }
-  , value   :  { type: Number }
-});
-
-var Measure = mongoose.model('Measure', MeasureSchema);
-
-
-var createMeasure = function(user, type, time, value) {
-  var msr = new Measure({
-    'user': user,
-    'type': type,
-    'time': time,
-    'value': value
-  });
-  msr.save();
-}
-
-
-//----------------------------------- Authentication
-
-var handleAuth = function(request, response) {
-  if(!isAuthenticated(request)) {
-    authenticateUser(request, response);
-  }
-}
-
-var authenticateUser = function(request, response) {
-  User.findOne({ 'email': request.param('email') } , function(err, user) {
-    if(err) {
-      console.log(err);
+//---------------------------------- Loggly
+var loggly = require('loggly');
+  var logglyconfig = {
+    subdomain: "sybeol",
+    auth: {
+      username: "nherment",
+      password: "6031769a"
     }
-    if(user == null) {
-      console.log('Could not find user with email['+request.param('email')+']');
-      //response.redirect('back', 403);
-    } else if(user.authenticate(request.param('password'))) {
-      request.session.authenticated = true;
-      request.session.email = user.email;
-      console.log("session email: "+request.session.email);
-      console.log('user with email['+request.param('email')+'] is now authenticated');
-    } else {
-      console.log('Failed authentication attempt for user with email['+request.param('email')+']');
-      //response.redirect('back', 403);
-    }
-  });
-}
+  };
+var client = loggly.createClient(logglyconfig);
 
-var isAuthenticated = function(req) {
-  console.log(req.session.authenticated);
-  return req.session.authenticated == true;
-}
+client.log('32be8efd-0733-4f6d-a898-52ead34af20a', 'server is started');
 
-//-----------------------------------
 
-/*var nhermentUser = new User({
-  email: 'nherment@gmail.com',
-  password: 'nherment'
-});
-nhermentUser.save();*/
-
-var createUser = function(email, password) {
-  var usr = new User({
-    'email': email,
-    'password': password
-  });
-  usr.save();
-}
 //----------------------------------- Server config
 
 
 var express = require('express');
+
+var RedisStore = require('connect-redis')(express);
 
 var app = express.createServer();
 
@@ -132,7 +39,7 @@ app.configure(function() {
     layout: false
   });
   app.use(express.cookieParser());
-  app.use(express.session({ secret: "qlasd33j0jfw3j" }));
+  app.use(express.session({ secret: "keyboard cat", store: new RedisStore }));
   app.use(app.router);
 });
 
@@ -167,7 +74,7 @@ app.post('/login', function(req, res) {
   var isJson = req.is('application/json');
   if(isJson) {
     console.log("login attempt for user["+ req.param('email') + "]");
-    handleAuth(req, res);
+    authSvc.handleAuth(req, res);
     res.send(200);
   } else {
     res.send(400);
@@ -176,7 +83,12 @@ app.post('/login', function(req, res) {
 
 app.get('/login/:email/:password', function(req, res) {
   console.log("login attempt for user["+ req.param('email') + "]");
-  handleAuth(req, res);
+  authSvc.handleAuth(req, res);
+  res.send(200);
+});
+
+app.get('/logout', function(req, res) {
+  req.session.destroy();
   res.send(200);
 });
 
@@ -190,12 +102,12 @@ console.log(req.session.count);
 
 //----------------------------------- Measure API
 
-app.put('/measure', function(req, res) {
+app.put('/measure/', function(req, res) {
   console.log("receive put reauest on /measure");
   var isJson = req.is('application/json');
   if(isJson) {
-    console.log("Creating user with email["+ req.body.email + "]");
-    createMeasure(req.body.email, req.body.password);
+    console.log("Creating new measure ["+ req.param('email') + "]");
+    createMeasure(req.param('email'), req.param('password'));
     res.send(200);
   } else {
     res.send(400);
